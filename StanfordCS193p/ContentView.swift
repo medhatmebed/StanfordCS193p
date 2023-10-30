bfda783471cc17920730e6d7c1c18449bbb2461f
medhatmebed <medhat.mebed@gmail.com>
2023-10-30 22:05:51 +0200

    Initial Commit

---
+++ StanfordCS193p/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StanfordCS193p
+//
+//  Created by Medhat Mebed on 10/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StanfordCS193p/StanfordCS193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordCS193pApp.swift
+//  StanfordCS193p
+//
+//  Created by Medhat Mebed on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct StanfordCS193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
